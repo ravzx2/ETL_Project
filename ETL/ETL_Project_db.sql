94b82425cb1a92039f3c6d3d5155fe148afc45cb
Robert Voss <bob.voss.21@gmail.com>
2018-12-22 13:20:40 -0600

    Add files via upload

---
+++ ETL/ETL_Project_db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE pollution_population_db;
+use pollution_population_db;
+select * from all_pollutants;
+#select * from num_bad_days;
+select * from state_populations;
+
+SELECT *
+FROM all_pollutants
+JOIN state_populations
+ON all_pollutants.`State Name` = state_populations.`State Name`
+#JOIN num_bad_days
+#ON num_bad_days.`State Name` = all_pollutants.`State Name`;
